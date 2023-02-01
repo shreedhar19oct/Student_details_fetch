88feef84bf81804ffc4d0591cc2f0cf7c3fdc9da
shreedhar19oct <shreedhar19oct@gmail.com>
2023-02-01 15:56:32 +0530

    intro_done

--- studentdetails/src/App.js
+++ studentdetails/src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Students from './Students';
+import Pagination from './Pagination';
+import Search from './Search';
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <div>Welcome to Student Details Website</div>
+    <Students />
+    <Pagination />
+    <Search />
+    </>
   );
-}
+};
 
 export default App;
