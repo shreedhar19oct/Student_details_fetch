ed5f27d213d2d12652fc252e085d14fdeacbf224
shreedhar19oct <shreedhar19oct@gmail.com>
2023-02-01 16:14:01 +0530

    calling_api_done

--- studentdetails/src/Students.js
+++ studentdetails/src/Students.js
@@ -1,9 +1,39 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 
 const Students = () => {
-  return (
-    <div>students</div>
-  );
+  let isLoading= true;
+
+  let API= "https://jsonplaceholder.typicode.com/users";
+
+  const fetchApiData = async(url) =>{
+    try{
+
+      const res = await fetch(url);
+      const data=await res.json();
+      console.log(data);
+      isLoading = false;
+    }
+    catch(error){
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    fetchApiData(API);
+  }, []);
+
+  if(isLoading){
+    return<>
+      <h1>Loading.......</h1>
+    </>
+  }
+  
+  return <>
+    <h2>Student Information Table</h2>
+  </>
+    // <div>students</div>
+
+
 };
 
 export default Students;
